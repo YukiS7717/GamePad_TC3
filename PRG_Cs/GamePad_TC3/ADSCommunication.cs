4ea4d6c51ea151c9b5ec7207786482a7fea9185d
YukiS7717 <y.seiza.lol@gmail.com>
2024-10-02 12:32:30 +0900

    新規作成

---
+++ PRG_Cs/GamePad_TC3/ADSCommunication.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using TwinCAT.Ads;
+
+namespace TwinCATUsbControllerApp
+{
+    public class ADSCommunication : IDisposable
+    {
+        private AdsClient adsClient;
+
+        public bool Connect(string amsNetId, int port)
+        {
+            try
+            {
+                adsClient = new AdsClient();
+                adsClient.Connect(amsNetId, port);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error connecting to ADS: {ex.Message}");
+                return false;
+            }
+        }
+
+        public void SendGamePadState(int controllerId, ST_GamePad state)
+        {
+            if (adsClient == null || !adsClient.IsConnected) return;
+
+            try
+            {
+                string varName = $"GVL.stGamePad{controllerId}";
+                uint variableHandle = adsClient.CreateVariableHandle(varName);
+                adsClient.WriteAny(variableHandle, state);
+                adsClient.DeleteVariableHandle(variableHandle);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending data to PLC: {ex.Message}");
+            }
+        }
+
+        public void Dispose()
+        {
+            adsClient?.Dispose();
+        }
+    }
+}
